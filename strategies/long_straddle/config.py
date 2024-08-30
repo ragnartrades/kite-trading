34b91c8400df7e56eca7b213ee32865007501a82
Subhram Subhrajyoti Rana <Subhram@MacBookPro.local>
2024-08-30 09:19:56 +0530

    nofty bank config

--- strategies/long_straddle/config.py
+++ strategies/long_straddle/config.py
@@ -4,16 +4,16 @@ from datetime import time
 SIMULATION_TYPE = 'FAKE'  # (FAKE, REAL)
 # fake is for paper trading and real is for real order in broker
 
-STOCK_NAME = 'NIFTY'  # it can be NIFTY also
+STOCK_NAME = 'BANKNIFTY'  # it can be NIFTY also
 LOT_QTY = 1
 TRADING_MONTH = "SEP"  # todo; important, make use of this while buying and selling options
 
-MAX_ENTRY_TIME = time(15, 25)
+MAX_ENTRY_TIME = time(12, 00)
 MAX_TRADE_TIME = time(15, 25)
 
 # SL_DEVIATION_TYPE = 'ABSOLUTE'  # (ABSOLUTE, PERCENTAGE)
-MAX_ENTRY_ATM_PRICE_DEVIATION = 48
+MAX_ENTRY_ATM_PRICE_DEVIATION = 5
 SL_DEVIATION = 10  # "point" . currently this is in ABSOLUTE point, Will see in future of PERCENTAGE
 # based calculation seems good
-MIN_PROFIT_PERCENTAGE = 1
-MAX_LOSS_PERCENTAGE = 5
+MIN_PROFIT_PERCENTAGE = 2
+MAX_LOSS_PERCENTAGE = 1
